b741ab89a83767b71fcd966254695a23b2ed6416
jenifler <96619243+jenifler@users.noreply.github.com>
2022-02-02 21:42:56 +0700

    Update bash.sh

--- Victory/bash.sh
+++ Victory/bash.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo apt install proxychains  -y 
-sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
-sudo sed -i 's/127.0.0.1/192.111.135.17/' /etc/proxychains.conf
-sudo sed -i 's/9050/18302/' /etc/proxychains.conf
 wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && cd TON-Stratum-Miner && ./TON-Stratum-Miner -w EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd -b cuda-18 -F 2048 --exclude-gpus 1 -r Victory

